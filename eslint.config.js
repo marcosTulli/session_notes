1381087a008210a44b4c26121131bfa5910017c4
Marcos <marcostulli@hotmail.com>
2025-11-15 19:27:21 +0100

    Update config

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,107 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import unusedImports from 'eslint-plugin-unused-imports';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
 
-export default defineConfig([
-  globalIgnores(['dist']),
+export default tseslint.config(
+  {
+    ignores: ['dist', 'node_modules', 'build', '.wrangler', 'vite.config.ts'],
+  },
+
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
   {
     files: ['**/*.{ts,tsx}'],
-    extends: [
-      js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs.flat.recommended,
-      reactRefresh.configs.vite,
-    ],
+    extends: [...tseslint.configs.recommendedTypeChecked],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+      },
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    plugins: {
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+      'unused-imports': unusedImports,
+      prettier: prettierPlugin,
+      import: importPlugin,
+    },
+    rules: {
+      'no-undef': 'error',
+      'react-refresh/only-export-components': 'warn',
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'error',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-enum-comparison': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/no-floating-promises': 'off',
+      'unused-imports/no-unused-vars': [
+        'error',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+          caughtErrors: 'none',
+        },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+      'import/named': 'error',
+      'import/namespace': 'error',
+      'import/default': 'error',
+      'import/export': 'error',
+      'import/no-unresolved': 'off',
+      'import/no-named-as-default': 'error',
+      // 'import/no-named-as-default-member': 'error',
+      'prettier/prettier': [
+        'error',
+        {
+          printWidth: 100,
+          singleQuote: true,
+          trailingComma: 'all',
+          tabWidth: 2,
+          endOfLine: 'auto',
+        },
+      ],
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+    },
+    settings: {
+      'import/parsers': {
+        '@typescript-eslint/parser': ['.ts', '.tsx'],
+      },
+      'import/resolver': {
+        typescript: {
+          alwaysTryTypes: true,
+          project: './tsconfig.json',
+        },
+      },
     },
   },
-])
+
+  {
+    files: ['./src/models/**/*'],
+    rules: {
+      'unused-imports/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'off',
+      'prettier/prettier': 'off',
+    },
+  },
+
+  prettierConfig,
+);
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,69 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react';
+import svgrPlugin from 'vite-plugin-svgr';
+import topLevelAwait from 'vite-plugin-top-level-await';
+import { version } from './package.json';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+    plugins: [
+    react(),
+    svgrPlugin({
+      svgrOptions: {
+        icon: true,
+      },
+    }),
+    topLevelAwait(),
+  ],
+  resolve: {
+    alias: [
+      { find: /^~(.*)$/, replacement: '$1' },
+      { find: '@', replacement: path.resolve(__dirname, 'src') },
+      {
+        find: '@components',
+        replacement: path.resolve(__dirname, 'src/components'),
+      },
+      { find: '@hooks', replacement: path.resolve(__dirname, 'src/hooks') },
+      { find: '@models', replacement: path.resolve(__dirname, 'src/models') },
+      { find: '@pages', replacement: path.resolve(__dirname, 'src/pages') },
+      { find: '@store', replacement: path.resolve(__dirname, 'src/store') },
+      { find: '@styles', replacement: path.resolve(__dirname, 'src/styles/index.scss') },
+      {
+        find: '@services',
+        replacement: path.resolve(__dirname, 'src/services'),
+      },
+      { find: '@theme', replacement: path.resolve(__dirname, 'src/theme') },
+      { find: '@config', replacement: path.resolve(__dirname, 'src/config') },
+      { find: '@utils', replacement: path.resolve(__dirname, 'src/utils') },
+    ],
+  },
+  server: {
+    port: 3000,
+    host: true,
+    strictPort: true,
+  },
+  define: {
+    'window.APP_VERSION': JSON.stringify(version),
+  },
+  test: {
+    environment: 'jsdom',
+    coverage: {
+      provider: 'istanbul',
+      include: ['**/models/*.js'],
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        quietDeps: true,
+        silenceDeprecations: ['legacy-js-api'],
+      },
+    },
+  },
 })
