362a95a3f344fcac32dfedf7a85fbeb35d333a1a
Marcos <marcostulli@hotmail.com>
2025-11-15 19:52:19 +0100

    Create Note Object

--- src/components/common/Sidebar/config/navigation.tsx
+++ src/components/common/Sidebar/config/navigation.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomeIcon from '@mui/icons-material/Home';
 import { Routes } from '@/models/enums';
+import TextSnippetIcon from '@mui/icons-material/TextSnippet';
 
 export interface NavigationItem {
   label: string;
@@ -10,4 +11,5 @@ export interface NavigationItem {
 
 export const navigationItems: NavigationItem[] = [
   { label: 'Home', icon: <HomeIcon />, href: Routes.Home },
+  { label: 'Notes', icon: <TextSnippetIcon />, href: Routes.Notes },
 ];
