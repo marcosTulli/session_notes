2bf6c9b3a7dfe6fd1b3139d5e3be092e646f1641
Marcos <marcostulli@hotmail.com>
2025-11-15 20:04:45 +0100

    Add documentation

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,8 +1,6 @@
 export const env = {
   API_KEY: import.meta.env.VITE_API_KEY,
   SUPABASE_PROJECT_URL: import.meta.env.VITE_SUPABASE_PROJECT_URL,
-  SUPABASE_USER: import.meta.env.VITE_SUPABASE_USER,
-  SUPABASE_PASSWORD: import.meta.env.VITE_PASSWORD,
 } as const;
 
 export const envJSON = JSON.stringify(env, null, 2);
